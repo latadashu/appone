fea4fb6f7af56d5ba7e8d296629c3d7b50a44cc8
邋遢大叔 <5165824+latadashu@user.noreply.gitee.com>
2020-04-06 11:10:20 +0800

    项目初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,11 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    'semi': ['off', 'always'],
+    'no-multiple-empty-lines': ['error', {
+      'max': 3
+    }],
+    'space-before-function-paren': ['off', 'always'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Element from 'element-ui'
+
+Vue.use(Element)
 
 Vue.config.productionTip = false
 
